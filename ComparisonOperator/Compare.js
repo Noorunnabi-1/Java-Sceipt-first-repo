c969cd879667642da27e7f84de5e98b5c2c80b60
Noorunnabi <167161370+Noorunnabi-1@users.noreply.github.com>
2024-06-03 23:44:30 -1100

    complete comparison operator

---
+++ ComparisonOperator/Compare.js
@@ -0,0 +1,27 @@
+// Comparison Operator
+let a = 5;
+let b = 8;    // "8" is eligble           ( js convert string to number and then compare )
+
+console.log("5 == 8   :", a == b);     // 5 == 8
+console.log("5 != 8   :", a != b);     // is 5 not equal to 8 
+
+console.log("a =", a, " , b =", b);
+// === it compare Equal to & type
+console.log("a === b  :", a === b);
+
+// !== it compare not Equal to & type
+console.log("a !== b  :", a !== b);
+
+console.log("a > b  :", a > b);
+
+console.log("a < b  :", a < b);
+
+console.log("a <= b  :", a <= b);
+
+console.log("a >= b  :", a >= b);
+
+
+
+
+
+
