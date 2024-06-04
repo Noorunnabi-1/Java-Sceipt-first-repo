7581edd2c5fcab595a9c0d39e9fb4394ca8eabc8
Noorunnabi <167161370+Noorunnabi-1@users.noreply.github.com>
2024-06-04 05:38:52 -1100

    complete Logical operator

---
+++ LogicalOperator/logical.js
@@ -0,0 +1,33 @@
+// Logical Operator
+//AND OPERATION------------------------------------------------- 
+let a = 6;
+let b = 5;
+
+// let cond1 = a > b;      (true)
+// let cond2 = a === b;    (false)
+// console.log("cond1 && cond2  : ", cond1 && cond2);
+//false
+
+
+// let cond1 = a > b;          //(true)
+// let cond2 = a === 6;        //(true)
+// console.log("cond1 && cond2  : ", cond1 && cond2);
+// True
+
+
+//console.log("cond1 && cond2  : ", a > b && a === 5);
+// False
+
+
+// OR OPERATION--------------------------------------------------
+console.log("cond1 || cond2  : ", a > b || a === 5);
+// True
+
+
+// Logical NOT 
+console.log(" !(6 > 5)  : ", !(a > b));
+//False
+
+
+
+
