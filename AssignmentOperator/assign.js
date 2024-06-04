31c0ac3756ed0c8607b172cdd36626d8ce4cb028
Noorunnabi <167161370+Noorunnabi-1@users.noreply.github.com>
2024-06-03 23:14:17 -1100

    complete assignment operator

---
+++ AssignmentOperator/assign.js
@@ -0,0 +1,32 @@
+// Assignment Operator
+// a+=1 : a = a + 1
+
+let a = 4;
+console.log("a+=1   :", a += 6);
+// now the value of a is 10 
+console.log("a*=2   :", a *= 2);
+// now a=20
+console.log("a/=4   :", a /= 4);
+// now a=5
+console.log("a%=2   :", a %= 2);
+// now a = 1
+console.log(a);
+console.log(a);
+
+console.log("a+6 = ", a = a + 6);
+// now a= 7
+console.log(a);
+console.log("a ** = 2   :", a **= 2);
+// a = 49
+console.log(a);
+console.log(++a);
+console.log(a);
+console.log(a++);
+console.log(a);
+
+
+
+
+
+
+
