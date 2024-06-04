d5bad2670de43b416694233eacf6644b5cf7059b
Noorunnabi <167161370+Noorunnabi-1@users.noreply.github.com>
2024-06-03 22:01:32 -1100

    add examples operator and conditional statement

---
+++ oprtrConStmnt/second.js
@@ -0,0 +1,40 @@
+//Arithematic Operator 
+let a = 7;
+let b = 10;
+
+console.log("a = ", a, "& b =", b);
+
+console.log(" a + b = ", a + b);
+
+console.log(" a - b = ", a - b);
+
+console.log(" a * b = ", a * b);
+
+console.log(" a / b = ", a / b);
+
+// Modulus operator
+a = 15;
+b = 4;
+console.log("a = ", a, "& b =", b);
+
+console.log(" a % b = ", a % b);
+
+// Exponential  a**b : means a to the power b 
+a = 2;
+b = 4;
+console.log("a = ", a, "& b =", b);
+console.log("a ** b = ", a ** b);
+
+// Unary Operator
+//Increment : a++ = a+1 same    (post increment)  (pre : ++a)
+a = 2;
+a++;   //a=a+1;
+
+console.log("a++ = ", a);
+
+//Decrement : a-- = a-1 same   (post increment)    (pre : --a)
+a = 2;
+a--;   //a=a-1;
+
+console.log("a-- = ", a);
+
