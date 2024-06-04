78e09bca55fae911e939f4489589b16fff8a7ea6
Noorunnabi <167161370+Noorunnabi-1@users.noreply.github.com>
2024-06-03 22:44:25 -1100

    rename folder name

---
+++ firstCode/first.js
@@ -0,0 +1,25 @@
+console.log("My first JavaScript code.");
+name = "Noorunnabi";
+console.log(name);
+age = 24;
+console.log(age);
+let fullName = "Noorun";
+console.log(fullName);
+const PI = 3.14;
+console.log(PI);
+
+const student = {
+    fullName: "Noorunnabi",
+    rollNumber: 191,
+    age: 20,
+    cgpa: 8.4,
+    isPass: true,
+};
+
+console.log(student.cgpa);
+
+student.age = student.age + 1;
+console.log(student.age);
+
+student.cgpa = student.cgpa - 1;
+console.log(student.cgpa);
