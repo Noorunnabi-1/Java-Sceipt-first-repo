321783d1d15380281b299d109de5b57120aaf986
Noorunnabi <167161370+Noorunnabi-1@users.noreply.github.com>
2024-06-30 21:07:05 -1100

    done Event Listener

--- Events/event.js
+++ Events/event.js
@@ -27,3 +27,32 @@ div3.onmouseover = (evt) => {
     console.log(evt.type);
     console.log(evt.clientX, evt.clientY);
 };
+
+// Event listionar
+let btn5 = document.querySelector("#btn5");
+btn5.addEventListener("click", (evt) => {
+    console.log("button5 was clicked!");
+    console.log(evt);
+    console.log(evt.type);
+});
+
+btn5.addEventListener("click", (evt) => {
+    console.log("button5 was clicked - handler1");
+});
+
+btn5.addEventListener("click", () => {
+    console.log("button5 was clicked - handler2");
+});
+
+// for remove button further
+const handler3 = () => {
+    console.log("button5 was clicked - handler3");
+};
+btn5.addEventListener("click", handler3);
+
+btn5.addEventListener("click", () => {
+    console.log("button5 was clicked - handler4");
+});
+
+// now we remove button 3
+btn5.removeEventListener("click", handler3);
