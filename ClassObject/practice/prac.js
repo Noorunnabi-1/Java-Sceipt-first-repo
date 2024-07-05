96660622611ec772e50bd1e04bb37e700ec32589
Noorunnabi <167161370+Noorunnabi-1@users.noreply.github.com>
2024-07-05 09:49:24 -1100

    complete practice in inheritance

--- ClassObject/practice/prac.js
+++ ClassObject/practice/prac.js
@@ -10,6 +10,15 @@ class User {
     }
 }
 
+class Admin extends User {
+    constructor(name, email) {
+        super(name, email);
+    }
+    editData() {
+        DATA = "Some new value";
+    }
+}
+
 let student1 = new User("Noorunnabi", "noor@gmail.com");
 let student2 = new User("Shriq", "shariq@gmail.com");
 let student3 = new User("Ali", "ali@gmail.com");
@@ -26,5 +35,12 @@ console.log(student5);
 let teacher1 = new User("Dean", "deancollege@gmail.com");
 console.log(teacher1.viewData());
 console.log(teacher1);
+
+let admin1 = new Admin("admin", "admin@college.com");
+console.log(admin1);
+console.log(DATA);
+console.log(admin1.editData());
+console.log(DATA);
+
 
 
