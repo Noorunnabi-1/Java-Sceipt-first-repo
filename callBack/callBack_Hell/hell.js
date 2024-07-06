e53ae0f606795f3b7f5b8c8ec747407569e7a049
Noorunnabi <167161370+Noorunnabi-1@users.noreply.github.com>
2024-07-06 05:19:25 -1100

    callBack hell complete now we are studyng promises

--- callBack/callBack_Hell/hell.js
+++ callBack/callBack_Hell/hell.js
@@ -17,3 +17,38 @@ for (let i = 0; i <= 5; i++) {
         console.log(j);
     }
 }
+
+
+// examples
+function getData(dataId) {
+
+    setTimeout(() => {
+        console.log("data", dataId);
+    }, 2000);
+}
+let dataAccess = getData();
+console.log(dataAccess);
+let idAccess = getData(21341);
+console.log(idAccess);
+
+
+// get data after 2sec delay 
+function getData1(dataId, getNextData) {
+    setTimeout(() => {
+        console.log("data", dataId);
+        if (getNextData) {
+            getNextData();
+        }
+    }, 3000);
+}
+//  this is a callBack hell  (nested call back)
+getData1(1, () => {
+    console.log("getting data2....");
+    getData1(2, () => {
+        console.log("getting data3....");
+        getData1(3, () => {
+            console.log("getting data4....");
+            getData1(4);
+        });
+    });
+});
